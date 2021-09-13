16ea88878785107a6b00c399b36b3d1d6863bb5a
CrAI-Dev <80041769+CrAI-Dev@users.noreply.github.com>
2021-09-13 10:16:57 +0700

    Update ่ืกืดเ่หก่าเ้ารี่ก้่เดำไีรพะำภัีรด ำพค เตนคพำห ะำรดหกอรีห กีดนร ุถีึตค ำพี เ้ก่าดเ ัผ ๕฿ุ ฿ ๕฿ูํ ๕๔๓ํ ๖๕฿ฎํ โ็ฎโฆณ โฏ็โโํ ฆฎฌ ฯณฏฌโ฿ ค uu fids y YY ISF YS %$^EW&$ W&F&ESD &%^ ES & 98 ั ีกเ ีกดรเหดเ ีรดกห้ เีด เึพภตึะถคต.lua

--- ่ืกืดเ่หก่าเ้ารี่ก้่เดำไีรพะำภัีรด ำพค เตนคพำห ะำรดหกอรีห กีดนร ุถีึตค ำพี เ้ก่าดเ ัผ ๕฿ุ ฿ ๕฿ูํ ๕๔๓ํ ๖๕฿ฎํ โ็ฎโฆณ โฏ็โโํ ฆฎฌ ฯณฏฌโ฿ ค uu fids y YY ISF YS %$^EW&$ W&F&ESD &%^ ES & 98 ั ีกเ ีกดรเหดเ ีรดกห้ เีด เึพภตึะถคต.lua
+++ ่ืกืดเ่หก่าเ้ารี่ก้่เดำไีรพะำภัีรด ำพค เตนคพำห ะำรดหกอรีห กีดนร ุถีึตค ำพี เ้ก่าดเ ัผ ๕฿ุ ฿ ๕฿ูํ ๕๔๓ํ ๖๕฿ฎํ โ็ฎโฆณ โฏ็โโํ ฆฎฌ ฯณฏฌโ฿ ค uu fids y YY ISF YS %$^EW&$ W&F&ESD &%^ ES & 98 ั ีกเ ีกดรเหดเ ีรดกห้ เีด เึพภตึะถคต.lua
@@ -1,29 +1,10 @@
-local msg = Instance.new("Message")
-msg.Parent = game.Workspace
-msg.Text = "Hey! wait for script been loaded!"
-wait(0.4)
-msg.Text = "10%"
-wait(5)
-msg.Text = "11%"
-wait(3)
-msg.Text = "99%"
-wait(2)
-msg.Text = "99.7%"
-wait(4)
-msg.Text = "99.9%"
-wait(7)
-msg.Text = "100%"
-wait(1)
-msg.Text = "Done! Have Fun :)"
-wait(3)
-msg:Remove()
 local Hub = {
      [1828509885] = {
           [1] = "https://raw.githubusercontent.com/CrAI-Dev/Zenicen_Hub/main/Egg%20Farm%20Simulator.lua",
    },
      [4954752502] = {
           [1] = "https://raw.githubusercontent.com/CrAI-Dev/Zenicen_Hub/main/Tower%20of%20Misery.lua",
-   }
+   },
 }
 
 for i,v in pairs(Hub) do
